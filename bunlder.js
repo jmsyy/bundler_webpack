a52a1acd950e68ca21486592222333a77ace54f6
jmsyy <jmsyuyang777@gmail.com>
2020-08-24 14:06:15 +0800

    - [miniPack] 小的 打包工具

--- bunlder.js
+++ bunlder.js
@@ -2,10 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const babylon = require('babylon')
 const traverse = require('babel-traverse').default;
+const babel = require('babel-core');
 
 let ID = 0;
 
-function createdAsset (fileName) {
+function createAsset (fileName) {
     const content = fs.readFileSync(fileName, 'utf-8');
     const ast = babylon.parse(content,{sourceType: "module"});
 
@@ -18,16 +19,20 @@ function createdAsset (fileName) {
     })
 
     const id = ID++;
+    const {code}  = babel.transformFromAst(ast, null, {
+      presets:['env'],
+    })
 
     return {
         id,
         fileName,
-        dependencies
+        dependencies,
+        code
     }
 }
 
-function createdGraph (entry) {
-    const mainAsset = createdAsset(entry);
+function createGraph (entry) {
+    const mainAsset = createAsset(entry);
 
     const queue = [mainAsset];
     
@@ -39,7 +44,7 @@ function createdGraph (entry) {
         asset.dependencies.forEach((relativePath) =>{
             const absolutePath =  path.join(dirname, relativePath);
 
-            const child =  createdAsset(absolutePath);
+            const child =  createAsset(absolutePath);
 
             asset.mapping[relativePath] = child.id;
 
@@ -52,6 +57,37 @@ function createdGraph (entry) {
 
 }
 
-const graph = createdGraph('./example/entry.js')
+function  bundle(graph) {
+  let modules = '';
 
-console.log(graph);
+  graph.forEach(mod => {
+    modules += `${mod.id}:[
+      function (require, module, exports) {
+        ${mod.code}
+      },
+      ${JSON.stringify(mod.mapping)},
+    ],`
+  });
+  const result = `
+    (function (modules){
+      function require(id){
+        const [fn,mapping] = modules[id];
+
+        function localRequire(relativePath){
+          return require(mapping[relativePath])
+        }
+
+        const module = { exports: {} };
+
+        fn(localRequire, module, module.exports);
+        return module.exports
+      }
+      require(0)
+    }({${modules}}));
+  `;
+  return result;
+}
+const graph = createGraph('./example/entry.js')
+const result = bundle(graph)
+console.log(result);
+
--- example/name.js
+++ example/name.js
@@ -1,1 +1,1 @@
-export const name = 'yuyang'
+export const name = 'miniPack'
